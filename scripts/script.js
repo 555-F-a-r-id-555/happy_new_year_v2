ec6ce522296bc9e217cd57377b548f70dc976ad3
Farid <makflay85zet@gmail.com>
2023-10-18 23:25:04 +0400

    added countdownTonewYear func

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,25 @@ function updateDateTime() {
     const currentDateTimeElement = document.getElementById("currentDate");
 
     currentDateTimeElement.textContent = dateTimeString;
+
+    // Вызов функции обратного отсчета до Нового Года и вывод результата
+    const countdownElement = document.getElementById("countdown");
+    countdownElement.textContent = countdownToNewYear();
 }
+
+function countdownToNewYear() {
+    const currentDate = new Date();
+    const newYear = new Date(currentDate.getFullYear() + 1, 0, 1); // Новый Год следующего года
+    const timeLeft = newYear - currentDate;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    return `${days} дней ${hours} часов ${minutes} минут ${seconds} секунд до Нового Года`;
+}
+
 
 setInterval(updateDateTime, 1000);
 
