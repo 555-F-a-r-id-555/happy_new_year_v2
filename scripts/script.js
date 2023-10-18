f8d7b3934b347a8ec5af87a01ee3599f003d4fef
Farid <makflay85zet@gmail.com>
2023-10-19 00:02:09 +0400

    added func-handle_key_press for message output2

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+let wishCounter = 1; // Счетчик для нумерации сообщений
+
+// Функция для обновления текущего времени, даты, обратного отсчета до Нового Года и обработки пожеланий пользователя
 function updateDateTime() {
     const currentDate = new Date();
     const day = currentDate.getDate();
@@ -9,8 +12,10 @@ function updateDateTime() {
 
     const dateTimeString = `${hours}:${minutes}:${seconds} - ${day}.${month}.${year}`;
 
+    // Получение элемента для вывода даты и времени
     const currentDateTimeElement = document.getElementById("currentDate");
 
+    // Установка текста элемента равным текущей дате и времени
     currentDateTimeElement.textContent = dateTimeString;
 
     // Вызов функции обратного отсчета до Нового Года и вывод результата
@@ -18,6 +23,32 @@ function updateDateTime() {
     countdownElement.textContent = countdownToNewYear();
 }
 
+// Функция для обработки нажатия клавиши Enter в поле ввода
+function handleKeyPress(event) {
+    if (event.key === "Enter") {
+        const newYearWishesInput = document.getElementById("newYearWishes");
+        const wishesText = newYearWishesInput.value;
+        if (wishesText.trim() !== "") {
+            // Создаем элемент <p> для отображения пожелания с номером и текстом
+            const wishElement = document.createElement("p");
+            wishElement.textContent = `${wishCounter}. ${wishesText}`;
+
+            // Получение элемента для вывода пожеланий
+            const wishesElement = document.getElementById("wishes");
+
+            // Добавляем новое пожелание в блок
+            wishesElement.appendChild(wishElement);
+
+            // Увеличиваем счетчик для следующего пожелания
+            wishCounter++;
+
+            // Очищаем поле ввода пожеланий
+            newYearWishesInput.value = "";
+        }
+    }
+}
+
+// Функция для расчета обратного отсчета до Нового Года
 function countdownToNewYear() {
     const currentDate = new Date();
     const newYear = new Date(currentDate.getFullYear() + 1, 0, 1); // Новый Год следующего года
@@ -31,8 +62,10 @@ function countdownToNewYear() {
     return `${days} дней ${hours} часов ${minutes} минут ${seconds} секунд до Нового Года`;
 }
 
+const newYearWishesInput = document.getElementById("newYearWishes");
+
+newYearWishesInput.addEventListener("keyup", handleKeyPress);
 
 setInterval(updateDateTime, 1000);
 
 updateDateTime();
-
